7bc769553333ca7c5254aecf96bb362e6daa17c8
Jonathan Yeboah <jonathan.yeboah@outlook.de>
2020-11-07 10:51:00 +0100

    * Added seeding for randomness * Added max tries

--- src/components/helperFunctions.js
+++ src/components/helperFunctions.js
@@ -1,5 +1,14 @@
-function getRandomInRange(from, to, fixed) {
-  return (Math.random() * (to - from) + from).toFixed(fixed) * 1
+import seedrandom from "seedrandom"
+
+function getRandomInRange(from, to, fixed, seed = '') {
+  let random
+  if (seed) {
+    let rng = seedrandom(seed)
+    random = rng()
+  } else {
+    random = Math.random()
+  }
+  return (random * (to - from) + from).toFixed(fixed) * 1
 }
 
 export {getRandomInRange}
